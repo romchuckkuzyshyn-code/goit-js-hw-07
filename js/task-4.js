bb28c700968a4483497ff84df89d683c31dda54f
Roman <romchuck.kuzyshyn@gmail.com>
2026-02-01 20:37:36 +0200

    finish

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form');
 
+// btn.preventDefaul();
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputEmail = event.currentTarget.elements.email.value;
+  const inputPassword = event.currentTarget.elements.password.value;
+  if (inputEmail.trim() === '' || inputPassword.trim() === '') {
+    alert('All form fields must be filled in');
+  }
+  const res = { inputEmail, inputPassword };
+  console.log(res);
+  event.currentTarget.reset();
+});
--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const btn = document.querySelector('.change-color');
+const changeColorSpan = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  changeColorSpan.textContent = color;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
