6736a02d017cd451fade5f6bff9ca1cdf5b06af0
Roman <romchuck.kuzyshyn@gmail.com>
2026-02-02 18:38:11 +0200

    fixed task4 and css

--- js/task-4.js
+++ js/task-4.js
@@ -3,12 +3,12 @@ const form = document.querySelector('.login-form');
 // btn.preventDefaul();
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const inputEmail = event.currentTarget.elements.email.value;
-  const inputPassword = event.currentTarget.elements.password.value;
-  if (inputEmail.trim() === '' || inputPassword.trim() === '') {
-    alert('All form fields must be filled in');
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  if (email.trim() === '' || password.trim() === '') {
+    return alert('All form fields must be filled in');
   }
-  const res = { inputEmail, inputPassword };
+  const res = { email, password };
   console.log(res);
   event.currentTarget.reset();
 });
